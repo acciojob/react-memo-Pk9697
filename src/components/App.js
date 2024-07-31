847f53b0e29c9dcde1c286031e20b703321c1ce9
Prashant Kumar <prashant.aman9@gmail.com>
2024-07-31 16:42:21 +0530

    Complete assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,69 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 
 function App() {
+	const [todos, setTodos] = useState([])
+	const [count, setCount] = useState(0)
+	const [tasks, setTasks] = useState([])
+	const [task, setTask] = useState('')
+	const expensiveCalculation = useMemo(() => {
+		return 1000000000
+	}, [])
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		setTasks((prev) => [...prev, task])
+		setTask('')
+	}
+
 	return (
-		<div>
+		<div id='main'>
+			<h1>React.useMemo</h1>
+			<h1>My todos</h1>
+			<ul>
+				{todos.map((todo, index) => (
+					<li key={index}>{todo}</li>
+				))}
+			</ul>
+			<button
+				id='add-todo-btn'
+				onClick={() => setTodos((prev) => [...prev, 'New Todo'])}
+			>
+				Add Todo
+			</button>
+			<hr />
 			<p>
-				Now I can render any React component on any DOM node I want using
-				ReactDOM.render
+				Count: {count}{' '}
+				<button id='incr-cnt' onClick={() => setCount(count + 1)}>
+					+
+				</button>
 			</p>
+
+			<h1>Expensive Calculation</h1>
+			<p>{expensiveCalculation}</p>
+			<hr />
+			<hr />
+			<h1>React.memo</h1>
+			<form onSubmit={handleSubmit}>
+				<input
+					id='skill-input'
+					type='text'
+					value={task}
+					onChange={(e) => setTask(e.target.value)}
+					required
+					minLength='5'
+				/>
+				<button
+					id='add-skill-btn'
+					// onClick={() => setTasks((prev) => [...prev, task])}
+				>
+					Add Skill
+				</button>
+			</form>
+			<ul>
+				{tasks.map((task, index) => (
+					<li key={index}>{task}</li>
+				))}
+			</ul>
 		</div>
 	)
 }
